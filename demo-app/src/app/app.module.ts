5ac6883568a6b28d757ab93bb3312db843d7fbd2
Eric Greene <eric@t4d.io>
2023-01-09 11:54:55 -0600

    add exercise 1

---
+++ demo-app/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { ColorToolModule } from './color-tool/color-tool.module';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, ColorToolModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ demo-app/src/app/color-tool/components/color-home/color-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-color-home',
+  templateUrl: './color-home.component.html',
+  styleUrls: ['./color-home.component.css']
+})
+export class ColorHomeComponent {
+
+}
