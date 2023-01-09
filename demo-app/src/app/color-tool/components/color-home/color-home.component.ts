a0ea62711c2b8eb85ea2ae5feb1c4afc108f87bd
Eric Greene <eric@t4d.io>
2023-01-09 13:41:42 -0600

    add exercise 2

--- demo-app/src/app/color-tool/components/color-home/color-home.component.ts
+++ demo-app/src/app/color-tool/components/color-home/color-home.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+// import { Color } from '../../models/colors';
+import { Color } from 'src/app/color-tool/models/colors';
+
 @Component({
   selector: 'app-color-home',
   templateUrl: './color-home.component.html',
-  styleUrls: ['./color-home.component.css']
+  styleUrls: ['./color-home.component.css'],
 })
 export class ColorHomeComponent {
+  headerText: string = 'Color Tool';
 
+  colors: Color[] = [
+    { id: 1, name: 'red', hexcode: 'ff0000' },
+    { id: 2, name: 'green', hexcode: '00ff00' },
+    { id: 3, name: 'blue', hexcode: '0000ff' },
+  ];
 }
