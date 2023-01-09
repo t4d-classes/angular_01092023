4adc6477c89f528ae78e629378e44552df4d357b
Eric Greene <eric@t4d.io>
2023-01-09 14:56:35 -0600

    add exercise 3

--- demo-app/src/app/color-tool/color-tool.module.ts
+++ demo-app/src/app/color-tool/color-tool.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ColorHomeComponent } from './components/color-home/color-home.component';
 
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     ColorHomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule, SharedModule
   ],
   exports: [ ColorHomeComponent ]
 })
